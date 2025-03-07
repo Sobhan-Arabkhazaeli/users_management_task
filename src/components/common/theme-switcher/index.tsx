e056eb67ea0715eb1371ce3d7c3dd454d1b3c653
sobhan369arab <sobhanarab30@gmail.com>
2025-03-07 18:53:48 +0330

    create Header

--- src/components/common/theme-switcher/index.tsx
+++ src/components/common/theme-switcher/index.tsx
@@ -1,10 +1,43 @@
-import { Button } from "@mui/material"
-// import AccessibleIcon from '@mui/icons-material/Accessible';
+import { Button } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme, TThemeSelector } from "../../../redux/slices/theme";
+import { useEffect } from "react";
+import BedtimeIcon from "@mui/icons-material/Bedtime";
+import LightModeIcon from "@mui/icons-material/LightMode";
 
 const ThemeSwitcher = () => {
-  return (
-    <Button variant="outlined"></Button>
-  )
-}
+  const theme = useSelector<TThemeSelector>(
+    (state) => state.persistedTheme.theme
+  );
+  const dispatch = useDispatch();
+  const iconProps = { height: "24px", fill: "#fff" };
 
-export default ThemeSwitcher
+  // Adding and removing the dark class from a document
+  useEffect(() => {
+    if (theme === true) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  return (
+    <Button
+      onClick={() => dispatch(toggleTheme(!theme))}
+      variant="contained"
+      sx={{
+        color: "white",
+        fontWeight: "bold",
+        borderRadius: "10px",
+      }}
+    >
+      {theme ? (
+        <BedtimeIcon sx={iconProps} />
+      ) : (
+        <LightModeIcon sx={iconProps} />
+      )}
+    </Button>
+  );
+};
+
+export default ThemeSwitcher;
--- src/components/partials/header/index.tsx
+++ src/components/partials/header/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import LogoImage from "../../../assets/shardi.png";
+
+import { Link } from "react-router-dom";
+import ThemeSwitcher from "../../common/theme-switcher";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="z-40 flex h-16 w-full rounded-2xl items-center justify-between px-5 py-2.5 shadow-sm bg-Navy">
+      <div className="flex gap-1 sm:gap-3 ">
+        <Link to={"/"}>
+          <img
+            src={LogoImage}
+            aria-label="logo png"
+            className=" !w-24  rounded-3xl"
+          />
+        </Link>
+      </div>
+      <div className="flex gap-1 sm:gap-4">
+        <ThemeSwitcher />
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
