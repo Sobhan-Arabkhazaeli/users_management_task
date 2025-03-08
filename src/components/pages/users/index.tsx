76ff3187cdfe5f96851e3fd59a1617ef2308c0e9
sobhan369arab <sobhanarab30@gmail.com>
2025-03-08 09:21:38 +0330

    create title section

--- src/components/pages/users/index.tsx
+++ src/components/pages/users/index.tsx
@@ -1,11 +1,12 @@
 import { useFetchUserQuery } from "../../../core/services/api/GetAllUsers";
+import TitleSection from "../../common/title-section";
 
 const UsersWrapper = () => {
   // API fetching and getting all users using RTK Query
-  const { data } = useFetchUserQuery();
+  const { data , isSuccess } = useFetchUserQuery();
 
-  return <div className="w-full lg:ml-36 ">
-    
+  return <div className="w-full lg:ml-36 p-5">
+    <TitleSection title="People" number={isSuccess ? data?.length : 0 }/>
   </div>;
 };
 
--- src/core/services/api/GetAllUsers.ts
+++ src/core/services/api/GetAllUsers.ts
@@ -6,10 +6,16 @@ export const GetAllUsers = createApi({
     baseUrl: "https://67c82dad0acf98d070854ab8.mockapi.io/api/v1/",
   }),
   endpoints: (builder) => ({
-    fetchUser: builder.query<{ users: TUserDetails[] }, void>({
-      query: () => `users`,
+    fetchUser: builder.query<
+      {
+        length: number;
+        users: TUserDetails[];
+      },
+      void
+    >({
+      query: () => "users",
     }),
   }),
 });
 
-export const { useFetchUserQuery } = GetAllUsers;  
+export const { useFetchUserQuery } = GetAllUsers;
