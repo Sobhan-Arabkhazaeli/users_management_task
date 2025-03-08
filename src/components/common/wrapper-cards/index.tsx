1cc29ee11e66d84ff222f3ce74c6fcfa88becfcb
sobhan369arab <sobhanarab30@gmail.com>
2025-03-08 18:53:45 +0330

    create user card skeleton

--- src/components/common/wrapper-cards/index.tsx
+++ src/components/common/wrapper-cards/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { TUserDetails } from "../../../core/types/common/user.type";
+import UserCard_Skeleton from "../skeleton/UserCard_Skeleton";
 import UserCard from "../user-card";
 
 interface IProp {
@@ -8,16 +9,28 @@ interface IProp {
   isSuccess?: boolean;
 }
 
-const WrapperCards: FC<IProp> = ({ usersData }) => {
-  return (
-    <div className="flex gap-5 flex-wrap justify-around">
-      {usersData !== undefined ? (
-        usersData?.map((item) => <UserCard key={item.id} {...item} />)
-      ) : (
-        <h1>User not found</h1>
-      )}
-    </div>
-  );
+const WrapperCards: FC<IProp> = ({ usersData, isLoading }) => {
+  if (isLoading) {
+    return (
+      <div className="flex gap-5 flex-wrap justify-around">
+        {[...Array(10)].map((_, index) => (
+          <UserCard_Skeleton key={index} />
+        ))}
+      </div>
+    );
+  } else {
+    if (usersData !== undefined) {
+      return (
+        <div className="flex gap-5 flex-wrap justify-around">
+          {usersData?.map((item) => (
+            <UserCard key={item.id} {...item} />
+          ))}
+        </div>
+      );
+    } else {
+      return <h1>User not found</h1>;
+    }
+  }
 };
 
 export default WrapperCards;
